5588ee818f21abc9c827ee1f4ab46d5f47dd6f7d
Paul Harding <pharding@kcionline.org>
2019-08-01 23:47:19 +0100

    Cosmetic change for admin users

--- views/site/home.php
+++ views/site/home.php
@@ -8,21 +8,25 @@
 $this->title = 'Sensus School Management System';
 
 use app\models\Form;
+use app\models\User;
 use yii\helpers\Html;
 use yii\helpers\BaseStringHelper;
 use yii\helpers\Url;
+
+/* @var $userIdentity User */
+$userIdentity = \Yii::$app->user->identity;
 
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Hi <?= BaseStringHelper::truncateWords(\Yii::$app->user->identity->full_name, 1, '') ?>,</h1>
+        <h1>Hi <?= BaseStringHelper::truncateWords($userIdentity->full_name, 1, '') ?>,</h1>
 
-        <p class="lead"><?= strtolower(\Yii::$app->user->identity->user_type) ?> at TKHSW. What do you want to do?</p>
+        <p class="lead"><?= strtolower($userIdentity->user_type) ?> at TKHSW. What do you want to do?</p>
 
         <hr class="my-4">
 
-        <h3>Take the register</h3>
+        <h3>Take<?=$userIdentity->isAdmin() ? '/adjust' : ''?>  the register</h3>
 
         <?php foreach ($forms as $model) {
             echo '<div class="btn-group" role="group">' . Html::a($model->name, Url::to(['attendance/create', 'form_id'=>$model->id]), ['class'=>'btn btn-lg btn-success']) . '</div>';
@@ -37,7 +41,7 @@ use yii\helpers\Url;
         </div>
 
         <div class="btn-group" role="group">
-            <button class="btn btn-warning" onclick="window.location='<?=$reportsUrl?>'">Run or print a report</button>
+            <button class="btn btn-warning" onclick="window.location='<?=$reportsUrl?>'">Custom report</button>
         </div>
 
     </div>
