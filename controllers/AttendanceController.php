6fe5f1c4b1b02b03102185b738712a24cd957e72
Paul Harding <pharding@kcionline.org>
2019-08-02 00:23:43 +0100

    Cleanup of unused code related to AttendanceController and views

--- controllers/AttendanceController.php
+++ controllers/AttendanceController.php
@@ -2,16 +2,12 @@
 
 namespace app\controllers;
 
-use app\models\AttendanceForm;
 use app\models\Form;
 use http\Exception\RuntimeException;
 use Yii;
 use app\models\Attendance;
-use app\models\AttendanceSearch;
 use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 
 /**
  * AttendanceController implements the CRUD actions for Attendance model.
@@ -26,60 +22,26 @@ class AttendanceController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::className(),
-                'except' => ['create'],
                 'rules' => [
                     // allow authenticated users
                     [
                         'allow' => true,
                         'roles' => ['@'],
-                        'matchCallback' => function ($rule, $action) {
-                            return Yii::$app->user !== null && Yii::$app->user->identity->isSuperUser();
-                        }
+//                        'matchCallback' => function ($rule, $action) {
+//                            return Yii::$app->user !== null && Yii::$app->user->identity->isSuperUser();
+//                        }
                     ],
                     // everything else is denied by default
-                ],
-            ],
-
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
                 ],
             ],
         ];
     }
 
     /**
-     * Lists all Attendance models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new AttendanceSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single Attendance model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Attendance record.
-     * If creation is successful, the browser will be redirected to the 'view' page.
+     * Creates (or updates) multiple Attendance records for a Form (class).
+     *
+     * TODO use transactions
+     *
      * @param integer $form_id of form (class)
      * @return mixed
      */
@@ -142,53 +104,4 @@ class AttendanceController extends Controller
         ]);
     }
 
-    /**
-     * Updates an existing Attendance model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Attendance model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Attendance model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Attendance the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Attendance::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
-    }
 }
