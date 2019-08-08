aa16478875ffe86949503a860cdbab1399e60b93
Paul Harding <pharding@kcionline.org>
2019-08-08 20:27:52 +0100

    Improvements for viewing on mobile devices.

--- views/site/home.php
+++ views/site/home.php
@@ -15,7 +15,7 @@ use yii\helpers\Url;
 $userIdentity = \Yii::$app->user->identity;
 
 ?>
-<div class="site-index">
+<div class="site-home">
 
     <div class="jumbotron">
         <h1>Hi <?= BaseStringHelper::truncateWords($userIdentity->full_name, 1, '') ?>,</h1>
@@ -27,15 +27,18 @@ $userIdentity = \Yii::$app->user->identity;
         <h3>Take<?=$userIdentity->isAdmin() ? '/adjust' : ''?>  the register</h3>
 
         <?php foreach ($forms as $model) {
-            echo '<div class="btn-group" role="group">' . Html::a($model->name, Url::to(['attendance/create', 'form_id'=>$model->id]), ['class'=>'btn btn-lg btn-success']) . '</div>';
+            echo '<div class="btn-group">' . Html::a($model->name, Url::to(['attendance/create', 'form_id'=>$model->id]), ['class'=>'btn btn-lg btn-success']) . '</div>';
         } ?>
 
         <hr class="my-4">
 
-        <h3>Check in/out visitors</h3>
+        <h3>Check in/out</h3>
 
-        <div class="btn-group" role="group">
-            <button class="btn btn-lg btn-primary" onclick="window.location='<?=Url::to(['visitor/index'])?>'">Manage Visitors</button>
+        <div class="btn-group">
+            <button class="btn btn-lg btn-primary" onclick="window.location='<?=Url::to(['visitor/index'])?>'">Visitors</button>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-lg btn-primary" onclick="window.location='<?=Url::to(['visitor/index'])?>'">Students</button>
         </div>
 
         <hr class="my-4">
